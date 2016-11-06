b49d4839ff35ffa5d13ceb1abd3155f6f8c78977
ktsn <ktsn55@gmail.com>
2016-11-06 22:03:54 +0900

    Fix tslint-eslint-rules path

---
+++ tslint.js
@@ -0,0 +1,111 @@
+const path = require('path')
+
+module.exports = {
+  rulesDirectory: [
+    path.join(path.dirname(require.resolve('tslint-eslint-rules')), 'dist/rules')
+  ],
+  rules: {
+    'no-internal-module': true,
+    'no-namespace': [true, 'allow-declarations'],
+    'no-reference': true,
+    'no-var-requires': true,
+    'typedef-whitespace': [
+      true,
+      {
+        'call-signature': 'nospace',
+        'index-signature': 'nospace',
+        parameter: 'nospace',
+        'property-declaration': 'nospace',
+        'variable-declaration': 'nospace'
+      },
+      {
+        'call-signature': 'onespace',
+        'index-signature': 'onespace',
+        parameter: 'onespace',
+        'property-declaration': 'onespace',
+        'variable-declaration': 'onespace'
+      }
+    ],
+    'no-arg': true,
+    'no-construct': true,
+    'no-duplicate-variable': true,
+    'no-empty': true,
+    'no-eval': true,
+    'no-unreachable': true,
+    'no-unsafe-finally': true,
+    'no-unused-expression': true,
+    'no-unused-variable': [
+      true,
+      {'ignore-pattern': '^_'}
+    ],
+    'no-var-keyword': true,
+    'switch-default': true,
+    'triple-equals': [
+      true,
+      'allow-null-check'
+    ],
+    'use-isnan': true,
+    eofline: true,
+    indent: [
+      true,
+      'spaces'
+    ],
+    'linebreak-style': [
+      true,
+      'LF'
+    ],
+    'no-trailing-whitespace': true,
+    'trailing-comma': [
+      true,
+      {
+        multiline: 'never',
+        singleline: 'never'
+      }
+    ],
+    'class-name': true,
+    'interface-name': [
+      true,
+      'never-prefix'
+    ],
+    'new-parens': true,
+    'no-angle-bracket-type-assertion': true,
+    'object-literal-key-quotes': [
+      true,
+      'as-needed'
+    ],
+    'object-literal-shorthand': true,
+    quotemark: [
+      true,
+      'single',
+      'jsx-double'
+    ],
+    semicolon: [
+      true,
+      'never'
+    ],
+    whitespace: [
+      true,
+      'check-branch',
+      'check-decl',
+      'check-operator',
+      'check-module',
+      'check-type',
+      'check-typecast'
+    ],
+    'no-constant-condition': true,
+    'no-control-regex': true,
+    'no-duplicate-case': true,
+    'no-empty-character-class': true,
+    'no-extra-semi': true,
+    'no-invalid-regexp': true,
+    'no-irregular-whitespace': true,
+    'no-regex-spaces': true,
+    'no-sparse-arrays': true,
+    'no-unexpected-multiline': true,
+    'no-multi-spaces': true,
+    'block-spacing': [
+      true,
+      'always'
+    ]
+  }
+}
